6edda64513d305da068aade4cd838b2fcdaa5490
haidar <haidarah134@gmail.com>
2024-11-07 07:52:29 +0700

    redirect keep param

---
+++ redirect.js
@@ -0,0 +1,8 @@
+// import ke page yang perlu redirect ke layout
+if (document.layout == undefined) {
+    let reg = /\/([^\/]*?)\.html/
+    let title = window.location.href.match(reg)[1]
+
+    const urlParams = new URLSearchParams(window.location.search);
+    window.location.href = `layout.html?page=${title}&${urlParams.toString()}`;
+}
