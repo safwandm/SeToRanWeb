eeb858f85091af04a35de4584bc15168d48bc788
M Galang Pradwita <mgalangpradwita16@gmail.com>
2024-12-27 20:11:35 +0700

    Connect Mitra page to database

---
+++ .svelte-kit/generated/client/nodes/11.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/(layout)/voucher/[voucherId]/+page.svelte";
---
+++ .svelte-kit/generated/client/nodes/13.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/(noLayout)/register/+page.svelte";
