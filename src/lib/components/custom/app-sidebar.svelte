620189e8d36cca46b50b0ef34e840040cd425077
haidar <haidarah134@gmail.com>
2025-03-19 11:42:49 +0700

    inisialisasi baru

---
+++ src/lib/components/custom/app-sidebar.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import Calendar from '@lucide/svelte/icons/calendar';
+	import House from '@lucide/svelte/icons/house';
+	import Inbox from '@lucide/svelte/icons/inbox';
+	import Search from '@lucide/svelte/icons/search';
+	import Settings from '@lucide/svelte/icons/settings';
+	import Users from '@lucide/svelte/icons/users';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+
+	// Menu items.
+	const items = [
+		{
+			title: 'Dashboard',
+			url: '#',
+			icon: House
+		},
+		{
+			title: 'Pelanggan',
+			url: '#',
+			icon: Users
+		},
+		{
+			title: 'Transaksi',
+			url: '#',
+			icon: Calendar
+		},
+		{
+			title: 'Diskon',
+			url: '#',
+			icon: Search
+		},
+		{
+			title: 'Mitra',
+			url: '#',
+			icon: Settings
+		},
+		{
+			title: 'Voucher',
+			url: '#',
+			icon: Settings
+		}
+	];
+</script>
+
+<Sidebar.Root>
+	<Sidebar.Content>
+		<Sidebar.Group>
+			<Sidebar.GroupLabel>Setoran</Sidebar.GroupLabel>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each items as item (item.title)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton>
+								{#snippet child({ props })}
+									<a href={item.url} {...props}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+	</Sidebar.Content>
+</Sidebar.Root>
--- src/routes/(layout)/pelanggan/[idPelanggan]/+page.ts
+++
@@ -1,7 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const load = (async ({ params }) => {
-    return {
-        idPelanggan: params.idPelanggan
-    };
-}) satisfies PageLoad;
